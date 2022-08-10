a1b8d25d61053fa206c720d251dcc765bbbc6608
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-10 19:45:47 +0530

    created views and endpoints

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
 dotenv.config()
+
+// Calling connection for DB
+connectDB()
+
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Logging the errors
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// HandleBars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'index', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+// Routes
+app.use('/', require('./routes/index'))
 
 app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} and app listening on port http://localhost:${port}`)
